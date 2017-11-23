4499177e2f806efd82454d7d42a1757ce3933dde
theMladyPan <stanislav@rubint.sk>
2017-11-23 22:24:00 +0100

    testing added along with coverage support

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, random
 from pimp_lib import *
 
 class TestStringMethods(unittest.TestCase):
@@ -9,6 +9,11 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(median([1,2,3,4,5]),3)
         self.assertEqual(median([1,2,4,5]),3)
         self.assertNotEqual(median([1,2,4,5]),2)
+    def test_multi_p(self):
+        img1 = openImage("img/obr.jpg")
+        img2 = multiproc(invert(openImage("img/obr.jpg")), exponential, args=())
+        save(img2,"img/out.jpg")
+        self.assertEqual(1,1)
 
 if __name__ == '__main__':
     unittest.main()
